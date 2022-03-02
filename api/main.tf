cbb7f5f1ed3f30eff4ea22da484830a212b87240
Nicholas Christian <nicholasdchristian@gmail.com>
2022-03-02 18:51:15 -0500

    fix(api): Remove environmental variable variable from api main.tf

--- api/main.tf
+++ api/main.tf
@@ -12,7 +12,6 @@ module "app_function_api_resource" {
   api_gateway_id          = module.api_gateway.api_gateway_id
   api_resource_name       = "appfunction"
   environment             = var.environment
-  environmental_variables = {}
   lambda_iam_policy   = data.aws_iam_policy_document.app_function_policy.json
   sns_alert_topic_arn = var.sns_alert_topic_arn
   sns_okay_topic_arn = var.sns_okay_topic_arn
